adc5f8dc422ccd7d659749c7d8884ec0af645846
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2022-03-18 11:17:49 +0000

    updates to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useState} from 'react';
 import './Navbar.css';
 import Logo from '../../assets/images/logo.png';
-import { CgMenuLeft, } from 'react-icons/cg';
+import { CgMenuLeft } from 'react-icons/cg';
 import { FaTimes } from 'react-icons/fa';
+import { FaTwitter, FaPinterestP } from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
 
 const Navbar = () => {
 
@@ -26,32 +28,39 @@ const Navbar = () => {
     let path = "#";
     
     return (
-        <nav className={ navbarSticky ? "container navbar active" : "container navbar"}>
-            <div className= "navbar-left">
-                <div className="logo">
-                    <img src={Logo} alt="logo" />
-                </div> 
-                <div className="logo-info">
-                    <h4>JAMES SPADER</h4>
-                    <h4><span>PHOTOGRAPHY</span></h4>
+        <div className="container">
+            <div className="navbar">
+                <div className="logo-container">
+                    <div className="logo">
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className="logo-info">
+                        <a href={path}><h4>JAMES SPADER</h4></a>
+                        <a href={path}><h4><span>PHOTOGRAPHY</span></h4></a>
+                    </div>
                 </div>
-            </div>
-            <div className="navbar-right">
-                {
-                    showMenu ?  <FaTimes onClick={toggleMenu}  className="close" size={30} color="#000"/> :  <CgMenuLeft className="menu" size={33} onClick={toggleMenu}  />
-                }
-                <div id={showMenu ? "circle-show" : "circle-hide"}>                    
-                </div>
-                <menu className="menu">
-                    <ul className= "menu-links" id={ showMenu ? "nav-menu-link-show" : "nav-menu-link-hide" }>
+                {showMenu && (
+                <nav>
+                    <ul>
                         <li><a href={path}>Home</a></li>
                         <li><a href={path}>Gallery</a></li>
                         <li><a href={path}>Latest</a></li>
                         <li><a href={path}>Contact</a></li>
+                        <div className="nav-socials">
+                            <li><a><FaTwitter size={40} color="#CE1924" /></a></li>
+                            <li><a><AiFillInstagram size={40} color="#CE1924"/></a></li>
+                            <li><a><FaPinterestP size={40} color="#CE1924" /></a></li>
+                        </div>
                     </ul>
-                </menu>    
+                
+                    <p class="back-text">MENU</p>
+                </nav>
+                )}
+                {
+                    showMenu ? <FaTimes id="menu-close"  onClick={toggleMenu}  className="close-icon" size={30} color="#000"/> : <CgMenuLeft id="menu-open" onClick={toggleMenu} className="menu-icon" size={30}  />
+                }
             </div>
-        </nav>
+        </div>
     )
 }
 
